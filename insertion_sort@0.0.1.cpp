17814f58b43036d688f8eade53cdbc7781dad24b
diffidentiae <32275273+diffidentiae@users.noreply.github.com>
2017-12-20 05:13:32 +0300

    Create insertion_sort@0.0.1.cpp

---
+++ insertion_sort@0.0.1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+std::istream & read(std::istream & stream, int & b) 
+{
+  int c;
+  if(stream>>c)
+  {
+    b=c;
+  }
+  else 
+		{
+			std::cout << "An error has occured while reading input data";
+		}
+	return stream;
+}
+  
+int main() 
+{
+  int a,i,j,point=0;
+  cin>>a;
+  int *mas=new int[a];
+  for (i=0;i<a;i++)
+    if (read(cin,mas[i]))
+      ;
+  
+  for(i=1;i<a;i++)
+  {
+    point=i;
+    for(j=i-1;j>=0;j--)
+      {
+        if (mas[point]<mas[j])
+        {
+          std::swap(mas[point],mas[j]);
+          point=j;
+        }  
+      }
+  }
+  cout<<"massiv: ";
+  for (i=0;i<a;i++)
+    cout<<mas[i]<<" ";
+}
