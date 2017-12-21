94a788950e1c4187332468095700d5045a4eb46e
diffidentiae <32275273+diffidentiae@users.noreply.github.com>
2017-12-21 06:51:31 +0300

    Update bubble_sort@0.0.1.cpp

--- bubble_sort@0.0.1.cpp
+++ bubble_sort@0.0.1.cpp
@@ -4,39 +4,40 @@ using namespace std;
 
 std::istream & read(std::istream & stream, int & n, int & b) 
 {
-  int c,l;
-  for (l=0;l<n;l++)
-  {
-    if(stream>>c)
-    {
-      b[l]=c;
-    }
-    else 
+	int c,l;
+  	for (l=0;l<n;l++)
+  	{
+    		if(stream>>c)
+    		{
+      			b[l]=c;
+    		}
+    		else 
 		{
 			std::cout << "An error has occured while reading input data";
 		}
-  }
+  	}
 	return stream;
 }
   
 int main() 
 {
-  int a,i,j;
-  cin>>a;
-  int *mas=new int[a];
-  if (read(cin,a,mas[a]))
-    for(i=0;i<a;i++)
-    {
-      for(j=1;j<a;j++)
-      {
-        if (mas[j-1]>mas[j])
-        {
-          std::swap(mas[j-1],mas[j]);
-        }  
-      }
-    }
-  cout<<"massiv: ";
-  for (i=0;i<a;i++)
-    cout<<mas[i]<<" ";
-  delete [] mas;
+  	int a,i,j;
+  	cin>>a;
+  	int *mas=new int[a];
+  	if (read(cin,a,mas[a]))
+    	for(i=0;i<a;i++)
+    	{
+      		for(j=1;j<a;j++)
+      		{
+        		if (mas[j-1]>mas[j])
+        		{
+          			std::swap(mas[j-1],mas[j]);
+        		}  
+      		}
+    	}
+  	
+	cout<<"massiv: ";
+  	for (i=0;i<a;i++)
+    		cout<<mas[i]<<" ";
+  	delete [] mas;
 }
