8f155b7338830686f29929e4ca8a46b9ae812732
diffidentiae <32275273+diffidentiae@users.noreply.github.com>
2018-03-05 22:03:10 +0300

    Update matrix_mod_0_0_1.cpp

--- matrix_mod_0_0_1.cpp
+++ matrix_mod_0_0_1.cpp
@@ -46,6 +46,11 @@ public:
 
 	matrix_t(matrix_t const & other)
 	{
+		for (unsigned int i = 0;i<rows_;i++)
+			{
+				delete[] data_[i];
+			}
+		delete[] data_;
 		data_ = new int *[other.rows_];
 		for (unsigned int i = 0; i < other.rows_; i++)
 		{
