e3b06c9c26dac66cfeb6ee716ddd973368b16f85
diffidentiae <32275273+diffidentiae@users.noreply.github.com>
2018-06-25 07:28:10 +0300

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,51 +1,53 @@
+
+
 #include <iostream>
 
 using namespace std;
 
 class tree_t
 {
-private: 
+private:
 	struct node_t 
-  {
+  	{
 		node_t * left_;
 		node_t * right_;
 		int key_;
 
 		node_t() 
-    {
+    		{
 			left_ = nullptr;
 			right_ = nullptr;
 			key_ = 0;
 		}
 
 		void print_(ostream & stream, int size_) const 
-    {
+    		{
 			if (left_ != nullptr) 
-      {
-        left_->print_(stream, size_ + 4);
-      }
+      			{
+        			left_->print_(stream, size_ + 4);
+      			}
 			for (int i = 0; i < size_ + 4; i++) 
-      {
-        stream << '-';
-      }
+      			{
+        			stream << '-';
+      			}
 			stream << key_ << endl;
 
 			if (right_ != nullptr)
-      {
-        right_->print_(stream, size_ + 4);
-      } 
+      			{
+        			right_->print_(stream, size_ + 4);
+      			} 
 		}
 
 		~node_t() 
-    {
+    		{
 			if (left_ != nullptr) 
-      {
-        delete left_;
-      }
+      			{
+        			delete left_;
+      			}
 			if (right_ != nullptr)
-      {
-        delete right_;
-      }
+      			{
+        			delete right_;
+      			}
 		}
 
 	};
@@ -54,55 +56,55 @@ private:
 
 public:
 	tree_t() 
-  {
+  	{
 		root_ = nullptr;
 	}
 
 	void insert(int key_) 
-  {
+  	{
 
 		if (root_ == nullptr) 
-    {
+    		{
 			root_ = new node_t;
 			root_->key_ = key_;
 		}
 		else 
-    {
+    		{
 			node_t * temp = root_;
 
 			while (root_->left_ != nullptr || root_->right_ != nullptr) 
-      {
+      			{
 				if (root_->key_ < key_ ) 
-        {
+        			{
 					if (root_->left_ != nullptr)
-          {
-            root_ = root_->left_;
-          }
+         	 			{
+            					root_ = root_->left_;
+          				}
 					else
-          {
-            break;
-          } 
+          				{
+            					break;
+          				} 
 				}
 				else if (root_->key_ > key_ ) 
-        {
+        			{
 					if (root_->right_ != nullptr) 
-          {
-            root_ = root_->right_;
-          }
-          else 
-          {
-            break;
-          }
+          				{
+            					root_ = root_->right_;
+          				}
+          				else 
+          				{
+            					break;
+          				}
 				}
 			}
 
 			if (root_->key_ < key_) 
-      {
+      			{
 				root_->left_ = new node_t;
 				root_->left_->key_ = key_;
 			}
 			else if (root_->key_ > key_) 
-      {
+      			{
 				root_->right_ = new node_t;
 				root_->right_->key_ = key_;
 			}
@@ -111,53 +113,53 @@ public:
 	}
 
 	bool find(int key_) const 
-  {
+  	{
 
 		if (root_ == nullptr) 
-    {
-      return false;
-    }
-    else 
-    {
+    		{
+      			return false;
+    		}
+    		else 
+    		{
 			node_t * temp = root_;
 
 			while (temp != nullptr) 
-      {
+      			{
 				if (temp->key_ == key_)
-        {
-          return true;
-        } 
+        			{
+          				return true;
+        			} 
 				else if (temp->key_ > key_) 
-        {
-          temp = temp->right_;
-        }
-        else if (temp->key_ < key_)
-        {
-          temp = temp->left_;
-        } 
+				{
+					temp = temp->right_;
+				}
+				else if (temp->key_ < key_)
+				{
+					temp = temp->left_;
+				} 
 			}
 			return false;
 		}
 	}
 
 	void print(ostream & stream) const 
-  {
+  	{
 		if (root_ == nullptr) 
-    {
+    		{
 			stream << "пустое дерево " << endl;
 		}
 		else 
-    {
+    		{
 			root_->print_(stream, 0);
 		}
 	}
 
 	~tree_t() 
-  {
+  	{
 		if (root_ != nullptr) 
-    {
-      delete root_;
-    }
+    		{
+      			delete root_;
+    		}
 	}
 };
 
@@ -169,7 +171,7 @@ int main()
 	int key_;
 
 	while (c != 'q') 
-  {
+  	{
 		switch (c)
 		{
 		case '+':
@@ -181,23 +183,25 @@ int main()
 		case '?':
 			cin >> key_;
 			if (tree_.find(key_))
-      {
-        cout << "true" << endl;
-      } 
+      			{
+        			cout << "true" << endl;
+      			} 
 			else
-      {
-        cout << "false" << endl;
-      } 
+			{
+				cout << "false" << endl;
+			} 
 			break;
 
 		case '=':
 			tree_.print(cout);
 			break;
 		
-    default:
-      cout << "NATHING HERE ZULUL" << endl;
-    }
-  cin >> c;  
+    		default:
+      			cout << "NATHING HERE ZULUL" << endl;
+    		}
+  		
+		cin >> c;
+
 	}
 	return 0;
 }
