ecb237801b68aa338252c9b18f1e3d506c0c39e5
Oleg Ershov <32275273+diffidentiae@users.noreply.github.com>
2021-12-09 18:28:22 +0300

    Update array3.cpp

--- array3.cpp
+++ array3.cpp
@@ -1,54 +1,90 @@
 #include <iostream>
-#define N 10
 
-using namespace std;
+class vector1 {
+	int * vectorData; //указатель на массив данных
+	int length; //число элементов
+	public:
+	vector1();
+	vector1(int N);
+	vector1(vector1 Copy);
+	~vector1();
+	void input();
+	void output();
+	int findMax();
+	vector1 operator + (vector1 rightVec);
+	vector1 operator - (vector1 rightVec);
+};
 
-std::istream & arrayinput(std::istream & stream,int array1[])
-{
-	char c;
-	for(unsigned int i = 0;i<N;i++)
-	{
-		if (stream >> array1[i])
-		{
-			c = getchar();
-			if (c == '\n'&&i != N - 1)
-			{
-				cout << "An error has occurred while reading numbers";
-				exit(0);
-			}
-		}
-		else
-		{
-			cout << "An error has occurred while reading numbers";
-		}
-	} 
-	return stream;
-}
+int main() {
+	int length1, length2;
+	std::cout << "Введите длину первого вектора: ";
+	std::cin >> length1;
+	vector1 a(length1);
+	std::cout << "Введите длину второго вектора: ";
+	std::cin >> length2;
+	vector1 b(length2);
 
-int main()
-{
-	int array1[N], array2[N];
-	if (arrayinput(cin, array1) && arrayinput(cin, array2))
-	{
-		;
-	}
-	else
-	{
-		cout << "An error has occurred while reading numbers";
-	}
-
-	int max = array1[0]+array2[0];
-	for (int i = 0;i < N;i++)
-	{
-		for (int j = i;j > -1;j--)
-		{
-			if ((array1[j]+array2[i]) > max)
-			{
-				max = array1[j] + array2[i];
-			}
-		}
-	}
 
 	
-	cout << max;
+	return 0;
 }
+
+vector1::vector1(int N) {
+	length  = N;
+	vectorData = new int[length];
+}
+
+vector1::~vector1() {
+	if (vectorData != nullptr)
+		delete(vectorData);
+}
+
+void vector1::input() {
+	if (length <= 0 || vectorData == nullptr) {
+		std::cout << "ошибка инициализации массива";
+	}
+	else {
+		for (int i = 0; i < length; i++) {
+			std::cout << "Введите " << length << " элементов через enter: \n";
+			std::cin >> vectorData[i]; 
+		}
+
+	}
+}
+
+void vector1::output(){
+	if (length <= 0 || vectorData == nullptr) {
+		std::cout << "ошибка инициализации массива";
+	}
+	else {
+		for (int i = 0; i < length; i++) {
+			std::cout << i+1 << ". " << vectorData[i] << "\n"; 
+		}
+
+	}
+}
+
+int vector1::findMax() {
+	if (length <= 0 || vectorData == nullptr) {
+		std::cout << "ошибка инициализации массива";
+	}
+	else {
+		int max = vectorData[0];
+		for (int i = 0; i < length; i++) {
+			if (max < vectorData[i])
+				max = vectorData[i];
+		}
+		return max;
+	}
+	return 0;
+}
+
+vector1 vector1::operator + (vector1 rightVec) {
+	if ()
+	vector1 result();
+	return ;
+}
+
+vector1 vector1::operator - (vector1 rightVec) {
+
+ }
