c7dcb65aada67d3a81b733b17062c07632817c31
Oleg Ershov <32275273+diffidentiae@users.noreply.github.com>
2021-12-15 01:17:34 +0300

    Update array3.cpp

--- array3.cpp
+++ array3.cpp
@@ -1,85 +1,38 @@
 #include <iostream>
-
-class vector1 {
-	int * vectorData; //указатель на массив данных
-	int length; //число элементов
-	public:
-	vector1();
-	vector1(int N);
-	~vector1();
-	int lengthPush();
-	void input();
-	void output();
-	int findMax();
-};
+#include <string>
 
 int main() {
-	int length1, length2;
-	std::cout << "Введите длину вектора: ";
-	std::cin >> length1;
-	vector1 a(length1);
-	a.input();
-	a.output();
-	std::cout << "Максимум: " << a.findMax();
-	return 0;
-}
-
-vector1::vector1(){
-	length = 0;
-	vectorData = nullptr;
-}
-
-vector1::vector1(int N) {
-	length  = N;
-	vectorData = new int[length];
-}
-
-vector1::~vector1() {
-	if (vectorData != nullptr)
-		delete(vectorData);
-}
-
-void vector1::input() {
-	if (length <= 0 || vectorData == nullptr) {
-		std::cout << "ошибка инициализации массива";
-	}
-	else {
-		std::cout << "Введите " << length << " элементов через enter: \n";
-		for (int i = 0; i < length; i++) {
-			
-			std::cin >> vectorData[i]; 
+	std::string inputString;
+	long long resultSum = 0;
+	while (std::cin >> inputString) {
+		int Temp = 0;
+		bool isNegative = false;
+		for (int i = 0; i < inputString.length(); i++) {
+			if (inputString[i] > 47 && inputString[i] < 58) {
+				Temp = Temp * 10 + int(inputString[i]) - 48;
+			}
+			else {
+				if (Temp != 0) {
+					if (isNegative == true) {
+						resultSum -= Temp;
+						isNegative = false;
+					}
+					else
+						resultSum += Temp;
+				}
+				Temp = 0;
+			}
+			if (i == inputString.length() - 1) {
+				if (isNegative == true) {
+					resultSum -= Temp;
+					isNegative = false;
+				}
+				else
+					resultSum += Temp;
+			}
+			if (inputString[i] == 45)
+				isNegative = true;
 		}
-
 	}
-}
-
-void vector1::output(){
-	if (length <= 0 || vectorData == nullptr) {
-		std::cout << "ошибка инициализации массива";
-	}
-	else {
-		for (int i = 0; i < length; i++) {
-			std::cout << i+1 << ". " << vectorData[i] << "\n"; 
-		}
-
-	}
-}
-
-int vector1::findMax() {
-	if (length <= 0 || vectorData == nullptr) {
-		std::cout << "ошибка инициализации массива";
-	}
-	else {
-		int max = vectorData[0];
-		for (int i = 0; i < lengthPush(); i++) {
-			if (max < vectorData[i])
-				max = vectorData[i];
-		}
-		return max;
-	}
-	return 0;
-}
-
-int vector1::lengthPush() {
-	return this->length;
+	std::cout << resultSum;
 }
